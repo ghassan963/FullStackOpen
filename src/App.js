9c3a15aa4ac1d8a6ed317e7de4fd1a06be99b592
Ghassan Abdulrazzak <59185135+ghassan963@users.noreply.github.com>
2022-04-24 07:11:36 +0300

    part 1 the javascript

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import react from "react";
+import React from "react";
 import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 const App = () => {
     const course = 'Half stack application development'
-    const part1 = 'Fundementals of React'
-    const exercises1 = 10
-    const part2 = 'useing props to passing data'
-    const exercises2 = 7
-    const part3 = 'state of a component'
-    const exercises3 = 14 
+    const part1 = {
+        name : 'Fundementals of React',
+        exercises : 10
+    }
+    const part2 = {
+        name : 'useing props to passing data',
+        exercises : 7
+    }
+    const part3 = {
+        name : 'state of a component',
+        exercises : 14
+    }
     return(
         <div>
             <Header name={course} />
-            <Content part={part1} exercise={exercises1} />
-            <Content part={part2} exercise={exercises2} />
-            <Content part={part3} exercise={exercises3} />
-            <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
+            <Content part={part1.name} exercise={part1.exercises} />
+            <Content part={part2.name} exercise={part2.exercises} />
+            <Content part={part3.name} exercise={part3.exercises} />
+            <Total exercise1={part1.exercises} exercise2={part2.exercises} exercise3={part3.exercises} />
         </div>
 )
 }
